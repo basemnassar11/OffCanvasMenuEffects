9905a5d811fdc5e52d1a4aab4c51d41d75ba3294
Basem Nassar <basem@wewebit.com>
2019-06-11 12:45:44 +0300

    menu

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+$(function(){
+
+    var $base = $('.menu-side').find('.base');
+    $('.base').treemenu({
+        delay: 200,
+        openActive: true,
+        
+      });
+
+
+    
+    console.log($base)
+    
+});
